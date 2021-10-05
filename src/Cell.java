2c17d050d8f172ed35daf68efa96300a55216aaf
Alex <xperiaz1981@gmail.com>
2021-10-05 03:44:54 +0300

    Создан класс, отвечающий за обработку действий с отдельной ячейкой

---
+++ src/Cell.java
@@ -0,0 +1,42 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+class Cell {
+    private final Color RED = Color.red;
+    private int x, y;
+    private Color color;
+
+    Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+        color = Color.gray;
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+
+    boolean checkHit(int x, int y) {
+        if (this.x == x && this.y == y) {
+            color = RED;
+            return true;
+        }
+        return false;
+    }
+
+    boolean isAlive() {
+        return color != RED;
+    }
+
+
+    void paint(Graphics g, int cellSize, boolean hide) {
+        if (!hide || (hide && color == RED)) {
+            g.setColor(color);
+            g.fill3DRect(x * cellSize + 1, y * cellSize + 1, cellSize - 2, cellSize - 2, true);
+        }
+    }
+}
